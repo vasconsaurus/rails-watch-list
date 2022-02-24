6a212ecf39fefb328a6e8b5763d019d6042640fd
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-02-24 17:06:03 -0300

    added controller and view dor bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,21 @@
 class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+
+    if @bookmark.save
+      redirect_to list_path(bookmark.list)
+    else
+      render :new
+    end
+  end
+
+  private
+  def bookmarks_params
+    params.require(:bookmark).permit(:comment)
+  end
+
 end
