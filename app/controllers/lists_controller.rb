de8697a006715e8b7c234376131abde9f5ad83a2
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-02-24 16:42:13 -0300

    added routes, controllers and views for lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,35 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show]
+
   def index
-    List.all
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to @list, notice: 'List was successfully created.'
+    else
+      render "lists/show"
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
