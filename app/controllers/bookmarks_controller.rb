bc7923e45c390127e78edddd04cc453b28d0d6e2
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-02-25 12:07:23 -0300

    added new/create bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,21 +1,31 @@
 class BookmarksController < ApplicationController
   def new
     @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
   end
 
   def create
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
 
     if @bookmark.save
-      redirect_to list_path(bookmark.list)
+      redirect_to list_path(@list)
     else
-      render :new
+      render 'lists/show'
     end
   end
 
+  def destroy
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@list)
+  end
+
   private
-  def bookmarks_params
-    params.require(:bookmark).permit(:comment)
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists
-  resources :bookmarks
+  resources :lists, only: [:show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:detroy]
+
+  root to: "lists#index"
 end
