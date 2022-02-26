a2216e9286e3c14810d816c96e37577cc0748001
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-02-26 20:33:08 -0300

    added review routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :lists, only: [:show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: [:new, :create]
   end
 
   resources :bookmarks, only: [:destroy]
+  resources :reviews, only: [:destroy]
 
   root to: "lists#index"
 end
