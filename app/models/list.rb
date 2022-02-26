d38a73ad145e1298c8dbe31e3a17ef556befe477
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-02-26 20:31:02 -0300

    added review model and migration

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,6 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
+  has_many :reviews, dependent: :destroy
   has_many :movies, through: :bookmarks
   validates :name, presence: true, uniqueness: true
   has_one_attached :photo
---
+++ app/models/review.rb
@@ -0,0 +1,8 @@
+class Review < ApplicationRecord
+  belongs_to :lists
+  validates :comment, presence: true
+  validates :rating, presence: true, numericality: {
+                                      greater_than_or_equal_to: 0,
+                                      less_than_or_equal_to: 5
+                                    }
+end
