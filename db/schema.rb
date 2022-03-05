6006d6a85f2c2a4428d188ff6b0188415e1034ba
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-03-05 15:31:12 -0300

    run delete movies rating migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_05_173815) do
+ActiveRecord::Schema.define(version: 2022_03_05_182611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +63,6 @@ ActiveRecord::Schema.define(version: 2022_03_05_173815) do
     t.string "title"
     t.text "overview"
     t.string "poster_url"
-    t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.float "score"
