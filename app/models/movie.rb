017c3d0036cb07c689caf09a6adf39d681e69068
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-03-05 15:18:51 -0300

    remove rating references

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
+  self.ignored_columns = %w[rating]
   has_many :bookmarks
   validates :title, :overview, presence: true, uniqueness: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :list
   validates :comment, presence: true
-  validates :rating, presence: true, numericality: {
+  validates :score, presence: true, numericality: {
                                       greater_than_or_equal_to: 0,
                                       less_than_or_equal_to: 5
                                     }
